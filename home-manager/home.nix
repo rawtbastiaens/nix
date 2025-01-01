28075ebad505a3da27130d2cbd95ea7cc714ae16
Rik Bastiaens <rbastiaens@ilionx.com>
2025-01-01 18:02:38 +0100

    initial commit

---
+++ home-manager/home.nix
@@ -0,0 +1,173 @@
+{ config, pkgs, ... }:
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "rba";
+  home.homeDirectory = "/home/rba";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.11"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = with pkgs; [
+    # System
+    docker
+
+    neofetch
+    ranger
+
+    zip
+    xz
+    unzip
+    p7zip
+
+    ripgrep
+    jq
+    yq-go
+    fzf
+
+    file
+    which
+    tree
+
+    # Markdown preview
+    glow
+    entr
+
+    # Monitoring/troubleshooting
+    htop
+    iotop
+    iftop
+    btop
+    strace
+    ltrace
+    lsof
+    ethtool
+    pciutils
+    usbutils
+
+    # Qtile
+    i3lock-fancy
+    firefox
+    rofi
+    dmenu
+    dunst
+    zellij
+    feh
+    arandr
+
+    # dotfile mgmt
+    chezmoi
+
+    # Editors
+    vscodium
+
+    newsboat
+    #neovim
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.j
+  #home.file = {
+  #  "nvim" = {
+  #    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/.config/nvim";
+  #    recursive = true;
+  #  };
+  #};
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/rba/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
+  
+  programs = {
+    alacritty = {
+      enable = true;
+    };
+    neovim = {
+      enable = true;
+      extraConfig = ''
+      	set number relativenumber
+        set tabstop=2 softtabstop=2 shiftwidth=2
+        set expandtab
+        syntax enable
+      '';
+    };
+    git = {
+      enable = true;
+      userName = "Rik Bastiaens";
+      userEmail = "rbastiaens@ilionx.com";
+    };
+    zsh = {
+      enable = true;
+      enableCompletion = false;
+
+      # Disable DPMS (screen auto turn-off)
+      loginExtra = "xset -dpms";
+      shellAliases = {
+        hmc = "nvim ~/.config/home-manager/home.nix";
+      	hms = "home-manager switch";
+      	nixbuild = "sudo nixos-rebuild switch";
+      	nixconf = "sudo nvim /etc/nixos/configuration.nix";
+        qtc = "nvim ~/.config/qtile/config.py";
+        g = "git";
+      };
+      oh-my-zsh = {
+        enable = true;
+        plugins = ["git" "systemd" "rsync" "kubectl" ];
+        theme = "robbyrussell";
+      };
+    };
+  };
+
+  services.picom.enable = true;
+  services.autorandr = {
+  	enable = true;
+  };
+  xsession.initExtra = "${pkgs.autorandr}/bin/autorandr --change";
+
+  #environment.etc = {
+  #  "xdg/gtk-3.0/settings.ini".text = ''
+  #    [Settings]
+  #    gtk-application-prefer-dark-theme=1
+  #  '';
+  #};
+
+  dconf.settings = {
+    
+  };
+
+  gtk.enable = true;
+  gtk.gtk3.extraConfig = {
+    gtk-application-prefer-dark-theme=1;
+  };
+
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # All options for home-manager:
+  # https://home-manager-options.extranix.com/?query=zsh&release=master
+}
