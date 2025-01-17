327e061f5e96a8dc727b78241e68c5bd0e7b826d
Rik Bastiaens <rbastiaens@ilionx.com>
2025-01-17 13:42:06 +0100

    nvim: disable netrw and use neo-tree as default file manager

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -102,7 +102,11 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
+
+-- Disable nvims own file browser
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
