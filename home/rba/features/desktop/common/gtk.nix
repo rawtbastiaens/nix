697ee23dd94e2f7c8f8329af828e5a531f4ac058
Rik Bastiaens <rbastiaens@ilionx.com>
2025-03-22 22:02:29 +0100

    nix: gtk config

--- home/rba/features/desktop/common/gtk.nix
+++ home/rba/features/desktop/common/gtk.nix
@@ -3,6 +3,13 @@
     enable = true;
     gtk3.extraConfig = {
       gtk-application-prefer-dark-theme=1;
+      gtk-cursor-theme-name="breeze_cursors";
+      gtk-cursor-theme-size=16;
+    };
+    gtk4.extraConfig = {
+      gtk-application-prefer-dark-theme=1;
+      gtk-cursor-theme-name="breeze_cursors";
+      gtk-cursor-theme-size=16;
     };
   };
 }
