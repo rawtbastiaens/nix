aa00c94e6e6dc13b7717372d2f482ae614cd4cb0
Rik Bastiaens <rbastiaens@ilionx.com>
2025-03-22 22:07:32 +0100

    nix: autorandr WIP

--- home/rba/features/desktop/common/randr.nix
+++ home/rba/features/desktop/common/randr.nix
@@ -1,12 +1,37 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
     arandr
-    autorandr
   ];
+
+  programs = {
+    autorandr = {
+      enable = true;
+    };
+  };
 
   services = {
     autorandr = {
       enable = true;
+      ignoreLid = true;
+      profiles = {
+        "dual_home" = {
+          config = {
+            HDMI-1 = {
+              enable = true;
+              primary = false;
+              position = "3440x0";
+              mode = "1920x1080";
+              rate = "60.00";
+            };
+            eDP-1 = {
+              enable = true;
+              primary = true;
+              position = "0x0";
+              mode = "3440x1440";
+            };
+          };
+        };
+      };
     };
   };
 
