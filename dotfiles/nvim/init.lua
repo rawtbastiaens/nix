914fed131377cc2954aebef24ca1867954dcfe78
Rik Bastiaens <rbastiaens@ilionx.com>
2025-04-16 18:16:01 +0200

    nvim: add go, bash, python language servers

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -624,7 +624,9 @@ require('lazy').setup({
       local servers = {
         ansiblels = {},
         -- clangd = {},
-        -- gopls = {},
+        gopls = {},
+        bashls = {},
+        jedi_language_server = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
---
+++ home/rba/features/desktop/common/steam.nix
@@ -0,0 +1,13 @@
+{
+  pkgs,
+  lib,
+  inputs,
+  ...
+}: {
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+  };
+}
