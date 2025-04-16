1b2abe0372c5d800c4c058535cb9c23dd5c14436
Rik Bastiaens <rbastiaens@ilionx.com>
2025-04-16 18:52:26 +0200

    nix: comment out some WIP autorandr stuff

--- home/rba/features/desktop/common/randr.nix
+++ home/rba/features/desktop/common/randr.nix
@@ -9,31 +9,28 @@
     };
   };
 
-  services = {
-    autorandr = {
-      enable = true;
-      ignoreLid = true;
-      profiles = {
-        "dual_home" = {
-          config = {
-            HDMI-1 = {
-              enable = true;
-              primary = false;
-              position = "3440x0";
-              mode = "1920x1080";
-              rate = "60.00";
-            };
-            eDP-1 = {
-              enable = true;
-              primary = true;
-              position = "0x0";
-              mode = "3440x1440";
-            };
-          };
-        };
-      };
-    };
-  };
+  #    ignoreLid = true;
+  #    profiles = {
+  #      "dual_home" = {
+  #        config = {
+  #          HDMI-1 = {
+  #            enable = true;
+  #            primary = false;
+  #            position = "3440x0";
+  #            mode = "1920x1080";
+  #            rate = "60.00";
+  #          };
+  #          eDP-1 = {
+  #            enable = true;
+  #            primary = true;
+  #            position = "0x0";
+  #            mode = "3440x1440";
+  #          };
+  #        };
+  #      };
+  #    };
+  #  };
+  #};
 
   # run autorandr --change on xsession init
   xsession.initExtra = "${pkgs.autorandr}/bin/autorandr --change";
