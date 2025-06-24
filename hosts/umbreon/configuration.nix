1de88454191ee85045501d040cc89082836f476a
Rik Bastiaens <mail@rikbastiaens.nl>
2025-06-24 15:47:49 +0200

    nix: diff config with running

--- hosts/umbreon/configuration.nix
+++ hosts/umbreon/configuration.nix
@@ -9,6 +9,7 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
@@ -109,6 +110,7 @@
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     neovim
+    lazygit
     mangohud
     goverlay
     discord
