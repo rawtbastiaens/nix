c42d81f4a8d0c9142a42aa60d96961fde5a1519b
Rik Bastiaens <rbastiaens@ilionx.com>
2025-06-24 12:40:16 +0200

    nvim: ltex language server

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -627,6 +627,7 @@ require('lazy').setup({
         gopls = {},
         bashls = {},
         jedi_language_server = {},
+        ltex = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
