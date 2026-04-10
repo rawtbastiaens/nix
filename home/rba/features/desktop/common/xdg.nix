4a7df70a5bffd8446b4e6ec63a45e93f27d39f46
Rik Bastiaens <rbastiaens@ilionx.com>
2026-04-10 15:01:15 +0200

    create desktop entry for neovim and restructure xdg settings

--- home/rba/features/desktop/common/xdg.nix
+++ home/rba/features/desktop/common/xdg.nix
@@ -1,18 +1,30 @@
 {
-  xdg.mimeApps = {
-    enable = true;
-    defaultApplications = {
-      "text/html" = ["firefox.desktop"];
-      "text/xml" = ["firefox.desktop"];
-      "application/pdf" = ["zathura"];
-      "application/yaml" = ["nvim"];
-      "application/json" = ["nvim"];
-      "text/markdown" = ["nvim"];
-      "image/png" = ["feh"];
-      "x-scheme-handler/http" = ["firefox.desktop"];
-      "x-scheme-handler/https" = ["firefox.desktop"];
-      "x-scheme-handler/discord" = ["vesktop.desktop"];
-      "x-scheme-handler/msteams" = ["teams-for-linux.desktop"];
+  xdg = {
+    desktopEntries = {
+      # Neovim desktop file
+      neovim = {
+        name = "Neovim";
+        genericName = "Text editor";
+        exec = "nvim %U";
+        terminal = true;
+        categories = [ "Network" "WebBrowser" ];
+      };
+    };
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "text/html" = ["firefox.desktop"];
+        "text/xml" = ["firefox.desktop"];
+        "application/pdf" = ["zathura"];
+        "application/yaml" = ["Neovim.desktop"];
+        "application/json" = ["Neovim.desktop"];
+        "text/markdown" = ["Neovim.desktop"];
+        "image/png" = ["feh"];
+        "x-scheme-handler/http" = ["firefox.desktop"];
+        "x-scheme-handler/https" = ["firefox.desktop"];
+        "x-scheme-handler/discord" = ["vesktop.desktop"];
+        "x-scheme-handler/msteams" = ["teams-for-linux.desktop"];
+      };
     };
   };
 }
