39f6152898eef25e530670dc95a9463a93769ad8
Rik Bastiaens <rbastiaens@ilionx.com>
2026-04-16 11:35:37 +0200

    enable hn-test config for aws again

--- home/rba/features/cli/aws.nix
+++ home/rba/features/cli/aws.nix
@@ -2,19 +2,19 @@
 {
   programs.awscli = {
     enable = true;
-    #settings = {
-    #  "profile hn-test" = {
-    #    "sso_session" = "hn-test";
-    #    "sso_account_id" = "977099024682";
-    #    "sso_role_name" = "SRFullAdmin";
-    #    "region" = "eu-central-1";
-    #    "output" = "json";
-    #  };
-    #  "sso-session hn-test" = {
-    #    "sso_start_url" = "https://vzlz.awsapps.com/start/#";
-    #    "sso_region" = "eu-central-1";
-    #    "sso_registration_scopes" = "sso:account:access";
-    #  };
-    #};
+    settings = {
+      "profile hn-test" = {
+        "sso_session" = "hn-test";
+        "sso_account_id" = "977099024682";
+        "sso_role_name" = "SRFullAdmin";
+        "region" = "eu-central-1";
+        "output" = "json";
+      };
+      "sso-session hn-test" = {
+        "sso_start_url" = "https://vzlz.awsapps.com/start/#";
+        "sso_region" = "eu-central-1";
+        "sso_registration_scopes" = "sso:account:access";
+      };
+    };
   };
 }
