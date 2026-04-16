19626a513594abd7d2636b17a6710134cf251592
Rik Bastiaens <rbastiaens@ilionx.com>
2026-04-16 13:59:14 +0200

    add hn-prod to aws client

--- home/rba/features/cli/aws.nix
+++ home/rba/features/cli/aws.nix
@@ -10,7 +10,19 @@
         "region" = "eu-central-1";
         "output" = "json";
       };
+      "profile hn-prod" = {
+        "sso_session" = "hn-prod";
+        "sso_account_id" = "707843606436";
+        "sso_role_name" = "SRFullAdmin";
+        "region" = "eu-central-1";
+        "output" = "json";
+      };
       "sso-session hn-test" = {
+        "sso_start_url" = "https://vzlz.awsapps.com/start/#";
+        "sso_region" = "eu-central-1";
+        "sso_registration_scopes" = "sso:account:access";
+      };
+      "sso-session hn-prod" = {
         "sso_start_url" = "https://vzlz.awsapps.com/start/#";
         "sso_region" = "eu-central-1";
         "sso_registration_scopes" = "sso:account:access";
