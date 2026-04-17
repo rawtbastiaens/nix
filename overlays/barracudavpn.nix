a4343cf6e8f6a01da8a05b1e2e3897fa8438a71c
Rik Bastiaens <rik.bastiaens@vodafoneziggo.com>
2026-04-17 13:29:06 +0200

    revert derivation

--- overlays/barracudavpn.nix
+++ overlays/barracudavpn.nix
@@ -1,41 +1,59 @@
-{
-  stdenv,
-  lib,
-  dpkg,
-  binutils,
-  findutils,
-  gzip,
-}:
+self: super: {
+  barracudavpn = super.stdenv.mkDerivation rec {
+    pname = "barracudavpn";
+    version = "1.0.0"; # Adjust to your deb version
 
-stdenv.mkDerivation rec {
-  pname = "barracudavpn";
-  version = "5.3.6";
+    src = ./barracudavpn.deb; # Path to your deb file
 
-  src = ./barracudavpn_5.3.6_amd64.deb;
+    # Required for extracting deb files
+    nativeBuildInputs = [
+      super.ar
+      super.gnutar
+      super.gzip
+    ];
 
-  nativeBuildInputs = [
-    dpkg
-    binutils
-    findutils
-    gzip
-  ];
+    # Runtime dependencies (adjust based on what you found)
+    buildInputs = [
+      super.stdenv.cc.cc.lib
+      super.openssl
+      super.curl
+      super.libnotify
+      # Add more as needed
+    ];
 
-  unpackPhase = ''
-    ar x $src
-    tar xzf data.tar.gz
-  '';
+    unpackPhase = ''
+      ar x $src
+      tar -xzf data.tar.gz
+    '';
 
-  installPhase = ''
-    mkdir -p $out
-    cp -r usr/bin $out/ 2>/dev/null || true
-    cp -r usr/lib $out/ 2>/dev/null || true
-    cp -r etc $out/ 2>/dev/null || true
-    cp -r opt $out/ 2>/dev/null || true
-  '';
+    installPhase = ''
+      mkdir -p $out/bin
+      cp usr/bin/barracudavpn $out/bin/
 
-  meta = with lib; {
-    description = "Barracuda VPN client";
-    license = licenses.unfree;
-    platforms = [ "x86_64-linux" ];
+      mkdir -p $out/lib
+      cp -r usr/lib/* $out/lib/ 2>/dev/null || true
+
+      mkdir -p $out/etc
+      cp -r etc/* $out/etc/ 2>/dev/null || true
+    '';
+
+    postFixup = ''
+      # Update library paths to point to Nix store
+      patchelf --set-interpreter ${super.stdenv.cc}/lib64/ld-linux-x86-64.so.2 \
+        $out/bin/barracudavpn || true
+
+      patchelf --set-rpath ${
+        super.lib.makeLibraryPath [
+          super.stdenv.cc.cc.lib
+          super.openssl
+          super.curl
+        ]
+      } $out/bin/barracudavpn || true
+    '';
+
+    meta = with super.lib; {
+      description = "Barracuda VPN client";
+      platforms = platforms.linux;
+    };
   };
 }
