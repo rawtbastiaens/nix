5dcc0165c340748e2523db18a966846c06122968
Rik Bastiaens <rik.bastiaens@vodafoneziggo.com>
2026-04-23 13:32:23 +0200

    xdg: syntax fixes of default apps

--- home/rba/features/desktop/common/xdg.nix
+++ home/rba/features/desktop/common/xdg.nix
@@ -13,17 +13,17 @@
     mimeApps = {
       enable = true;
       defaultApplications = {
-        "text/html" = ["firefox.desktop"];
-        "text/xml" = ["firefox.desktop"];
-        "application/pdf" = ["zathura"];
-        "application/yaml" = ["Neovim.desktop"];
-        "application/json" = ["Neovim.desktop"];
-        "text/markdown" = ["Neovim.desktop"];
-        "image/png" = ["feh"];
-        "x-scheme-handler/http" = ["firefox.desktop"];
-        "x-scheme-handler/https" = ["firefox.desktop"];
-        "x-scheme-handler/discord" = ["vesktop.desktop"];
-        "x-scheme-handler/msteams" = ["teams-for-linux.desktop"];
+        "text/html" = [ "firefox.desktop" ];
+        "text/xml" = [ "firefox.desktop" ];
+        "application/pdf" = [ "zathura" ];
+        "application/yaml" = [ "Neovim.desktop" ];
+        "application/json" = [ "Neovim.desktop" ];
+        "text/markdown" = [ "Neovim.desktop" ];
+        "image/png" = [ "feh" ];
+        "x-scheme-handler/http" = [ "firefox.desktop" ];
+        "x-scheme-handler/https" = [ "firefox.desktop" ];
+        "x-scheme-handler/discord" = [ "vesktop.desktop" ];
+        "x-scheme-handler/msteams" = [ "teams-for-linux.desktop" ];
       };
     };
   };
