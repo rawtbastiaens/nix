af00059afa680b4216ed471d9a6064b28eabdf91
Rik Bastiaens <rik.bastiaens@vodafoneziggo.com>
2026-04-24 12:39:39 +0200

    install: nix-ld, for running unpacked binaries

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -2,32 +2,32 @@
 {
   system.stateVersion = "25.05"; # Did you read the comment?
 
-  imports = 
-    [
-      ./audio.nix
-      ./bluetooth.nix
-      ./btrfs.nix
-      ./buildtools.nix
-      ./docker.nix
-      ./gpg.nix
-      ./hyprland.nix
-      ./fonts.nix
-      ./k8s.nix
-      ./latex.nix
-      ./libvirtd.nix
-      ./locale.nix
-      ./networking.nix
-      ./nix-settings.nix
-      ./nix-tools.nix
-      ./rustdesk.nix
-      ./security.nix
-      ./sops.nix
-      ./ssh.nix
-      ./users.nix
-      ./vagrant.nix
-      ./x11.nix
-      ./zsh.nix
-    ];
+  imports = [
+    ./audio.nix
+    ./bluetooth.nix
+    ./btrfs.nix
+    ./buildtools.nix
+    ./docker.nix
+    ./gpg.nix
+    ./hyprland.nix
+    ./fonts.nix
+    ./k8s.nix
+    ./latex.nix
+    ./ld.nix
+    ./libvirtd.nix
+    ./locale.nix
+    ./networking.nix
+    ./nix-settings.nix
+    ./nix-tools.nix
+    ./rustdesk.nix
+    ./security.nix
+    ./sops.nix
+    ./ssh.nix
+    ./users.nix
+    ./vagrant.nix
+    ./x11.nix
+    ./zsh.nix
+  ];
   environment.systemPackages = with pkgs; [
   ];
 }
