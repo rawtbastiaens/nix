aece08620392e8b6c97fb5d4c966f4b7a1864f7f
Rik Bastiaens <rik.bastiaens@vodafoneziggo.com>
2026-04-29 10:08:55 +0200

    fix: change repo path, install hyprbars

--- home/rba/features/desktop/hypr/default.nix
+++ home/rba/features/desktop/hypr/default.nix
@@ -3,7 +3,8 @@
   wayland.windowManager.hyprland = {
     enable = true;
     plugins = [
-      inputs.hyprland-plugins.packages.${pkgs.system}.hyprexpo
+      inputs.hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.hyprexpo
+      inputs.hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.hyprbars
     ];
   };
 
