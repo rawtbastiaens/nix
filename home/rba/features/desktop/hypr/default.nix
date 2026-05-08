e829a41ea06c39a70831a375945f648d82025097
Rik Bastiaens <rik.bastiaens@vodafoneziggo.com>
2026-05-08 12:51:02 +0200

    hm: comment out bg_col value for hyprexpo config

--- home/rba/features/desktop/hypr/default.nix
+++ home/rba/features/desktop/hypr/default.nix
@@ -230,7 +230,7 @@ in
         hyprexpo = {
           columns = 3;
           gap_size = 5;
-          bg_col = "rgb (111111)";
+          # bg_col = "rgb (111111)";
           workspace_method = "first 1";
         };
       };
