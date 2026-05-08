c2b6234812066d7028337e1a9f8722c6270fb099
Rik Bastiaens <rik.bastiaens@vodafoneziggo.com>
2026-05-08 11:04:45 +0200

    hm: hypr - split monitor settings per host

--- home/rba/bulbasaur.nix
+++ home/rba/bulbasaur.nix
@@ -18,8 +18,17 @@
     ./features/desktop/optional/noisetorch.nix
     ./features/desktop/optional/obsidian.nix
     ./features/desktop/optional/wayland.nix
+    ./features/desktop/hypr
     ./features/desktop/optional/mail.nix
     ./features/desktop/optional/calibre.nix
+  ];
+
+  wayland.windowManager.hyprland.settings.monitor = [
+    "eDP-1, 1920x1080, 0x0, 1"
+    "desc:Samsung Electric Company LC34G55T HNBX800474, 3440x1440,1920x0,1"
+    "desc:Dell Inc. DELL P3424WE D8F66T3, 3440x1440,0x-1440,1"
+    "desc:LG Electronics LG ULTRAWIDE 0x00055CE1, 3440x1440, auto-center-left, 1"
+    ", preferred, auto, 1"
   ];
 
   home.file = {
