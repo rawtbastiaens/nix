cf25f2c6ceb37e00c92d7b8f7a364e014e299583
Rik Bastiaens <rik.bastiaens@vodafoneziggo.com>
2026-05-08 12:50:01 +0200

    hm: wrap color keyword in quotes

--- home/rba/features/desktop/hypr/default.nix
+++ home/rba/features/desktop/hypr/default.nix
@@ -52,7 +52,7 @@ in
           enabled = true;
           range = 4;
           render_power = 3;
-          color = "rgba(1a1a1aee)";
+          "color" = "rgba(1a1a1aee)";
         };
 
         blur = {
